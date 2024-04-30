f79e41b7c8bdec605a66505658cfa57492d1df46
Rafi Ferdos <rafiferdos@gmail.com>
2024-04-30 10:41:31 +0600

    Add POST endpoint for art_and_crafts in index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 require('dotenv').config()
+
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -24,13 +25,22 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const dataCollection = client.db('dataDB').collection('data')
+
+    app.post('/art_and_crafts', async (req, res)=> {
+        const newData = req.body;
+        console.log(newData);
+        const result = await dataCollection.insertOne(newData);
+        res.send(result)
+    })
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
